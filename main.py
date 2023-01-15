b2d77805ee34e390c41643be504503f08ea437a6
sudharaD <sudharad7@gmail.com>
2023-01-15 21:32:32 +0530

    user class - cunstructor created

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 class User:
-    pass
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
 
 
-user_1 = User()
+user_1 = User("001", "Isanka")
+
+print(user_1.followers)
 
